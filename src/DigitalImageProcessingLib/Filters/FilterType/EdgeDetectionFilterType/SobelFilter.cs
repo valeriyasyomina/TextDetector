8fd1353747737b81c9bc2694f1c73005fdb1d193
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-12 09:48:53 +0300

    Sobel filter changed

--- src/DigitalImageProcessingLib/Filters/FilterType/EdgeDetectionFilterType/SobelFilter.cs
+++ src/DigitalImageProcessingLib/Filters/FilterType/EdgeDetectionFilterType/SobelFilter.cs
@@ -10,14 +10,15 @@ namespace DigitalImageProcessingLib.Filters.FilterType.EdgeDetectionFilterType
 {
     public class SobelFilter: EdgeDetectionFilter
     {
-        static protected int FilterSize = 3;
+        static protected int FILTER_SIZE = 3;
+        static protected int TRESHOLD = 128 * 128;
         public SobelFilter()
         {
             try
             {
-                this.Size = FilterSize;
+                this.Size = FILTER_SIZE;
                 this.Gx = new int[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
-                this.Gy = new int[,] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
+                this.Gy = new int[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };   // { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } 
             }
             catch (Exception exception)
             {
@@ -40,50 +41,45 @@ namespace DigitalImageProcessingLib.Filters.FilterType.EdgeDetectionFilterType
                 if (image.Width < this.Size)
                     throw new ArgumentException("Image width must be >= filter size");
 
+                GreyImage copyImage = (GreyImage)image.Copy();
+                if (copyImage == null)
+                    throw new NullReferenceException("Null copy image in Apply");
+
                 int lowIndex = Size / 2;
                 int highIndexI = image.Height - lowIndex;
                 int highIndexJ = image.Width - lowIndex;
                 for (int i = lowIndex; i < highIndexI; i++)
                     for (int j = lowIndex; j < highIndexJ; j++)
                     {
-                        int gradientStrengthX = image.Pixels[i - 1, j - 1].Color.Data * Gx[0, 0] +
-                            image.Pixels[i - 1, j + 1].Color.Data * Gx[0, 2] + image.Pixels[i, j - 1].Color.Data * Gx[1, 0] +
-                            image.Pixels[i, j + 1].Color.Data * Gx[1, 2] + image.Pixels[i + 1, j - 1].Color.Data * Gx[2, 0] +
-                            image.Pixels[i + 1, j + 1].Color.Data * Gx[2, 2];
+                        int gradientStrengthY = copyImage.Pixels[i - 1, j - 1].Color.Data * Gy[0, 0] +
+                          copyImage.Pixels[i - 1, j].Color.Data * Gy[0, 1] + copyImage.Pixels[i - 1, j + 1].Color.Data * Gy[0, 2] +
+                          copyImage.Pixels[i + 1, j - 1].Color.Data * Gy[2, 0] + copyImage.Pixels[i + 1, j].Color.Data * Gy[2, 1] +
+                          copyImage.Pixels[i + 1, j + 1].Color.Data * Gy[2, 2];
 
-                        int gradientStrengthY = image.Pixels[i - 1, j - 1].Color.Data * Gy[0, 0] +
-                            image.Pixels[i - 1, j].Color.Data * Gy[0, 1] + image.Pixels[i - 1, j + 1].Color.Data * Gy[0, 2] +
-                            image.Pixels[i + 1, j - 1].Color.Data * Gy[2, 0] + image.Pixels[i + 1, j].Color.Data * Gy[2, 1] +
-                            image.Pixels[i + 1, j + 1].Color.Data * Gy[2, 2];
+                        int gradientStrengthX = copyImage.Pixels[i - 1, j - 1].Color.Data * Gx[0, 0] +
+                          copyImage.Pixels[i - 1, j + 1].Color.Data * Gx[0, 2] + copyImage.Pixels[i, j - 1].Color.Data * Gx[1, 0] +
+                          copyImage.Pixels[i, j + 1].Color.Data * Gx[1, 2] + copyImage.Pixels[i + 1, j - 1].Color.Data * Gx[2, 0] +
+                          copyImage.Pixels[i + 1, j + 1].Color.Data * Gx[2, 2];
 
-                        image.Pixels[i, j].Gradient.Strength = (int)Math.Sqrt(gradientStrengthX * gradientStrengthX + gradientStrengthY * gradientStrengthY);
+                        int gradientStrengthSqr = gradientStrengthX * gradientStrengthX + gradientStrengthY * gradientStrengthY;
+                        int gradientStrength = (int)Math.Sqrt((double)gradientStrengthSqr);
+                        image.Pixels[i, j].Gradient.Strength = gradientStrength;
+
+                        if (gradientStrengthSqr > TRESHOLD)
+                            image.Pixels[i, j].Color.Data = (byte)ColorBase.MIN_COLOR_VALUE;
+                        else
+                            image.Pixels[i, j].Color.Data = (byte)ColorBase.MAX_COLOR_VALUE;
 
                         if (gradientStrengthX == 0)
-                            if (gradientStrengthY == 0)
-                            {
-                                image.Pixels[i, j].Gradient.Angle = 0;
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE_0;
-                            }
-                            else
-                            {
-                                image.Pixels[i, j].Gradient.Angle = 90;
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE_90;
-                            }
-                        else
                         {
-                            int gradientDirection = (int)((Math.Atan((double)gradientStrengthY / gradientStrengthX)) * (180 / Math.PI));                           
-                            image.Pixels[i, j].Gradient.Angle = gradientDirection;
-
-                            if ((gradientDirection >= -90 && gradientDirection <= -68) || (gradientDirection > 67 && gradientDirection <= 90))
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE_90;
-                            else if (gradientDirection > -68 && gradientDirection <= -23)
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE_135;
-                            else if (gradientDirection > -23 && gradientDirection <= 22)
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE_0;
-                            else if (gradientDirection > 22 && gradientDirection <= 67)
-                                image.Pixels[i, j].Gradient.RoundGradientDirection = GradientData.RoundGradientDirection.DEGREE__45;
-                        }                  
-                    }               
+                            if (gradientStrengthY == 0)
+                                image.Pixels[i, j].Gradient.Angle = 0;
+                            else
+                                image.Pixels[i, j].Gradient.Angle = 90;
+                        }
+                        else
+                            image.Pixels[i, j].Gradient.Angle = (int)((Math.Atan((double)gradientStrengthY / gradientStrengthX)) * (180 / Math.PI));
+                    }
             }
             catch (Exception exception)
             {
