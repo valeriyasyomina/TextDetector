19c164840d6fa5c5d1cbc02d0a0785fef531b339
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-12 09:47:46 +0300

    GreyImage - Bitmap convertor added

---
+++ src/TextDetector/Convertor/IBitmapConvertor.cs
@@ -0,0 +1,18 @@
+﻿using DigitalImageProcessingLib.ImageType;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDetector.Convertor
+{
+    interface IBitmapConvertor
+    {
+        Bitmap ToBitmap(GreyImage image);
+        Bitmap ToBitmap(RGBImage image);
+        GreyImage ToGreyImage(Bitmap bitmap);
+        RGBImage ToRGBImage(Bitmap bitmap);
+    }
+}
