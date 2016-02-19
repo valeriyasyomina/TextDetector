a00d41eb45d6210f56050cbe1a07ae15b499a90d
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-19 16:41:36 +0300

    CC alg class added

---
+++ src/DigitalImageProcessingLib/Interface/IConnectedComponent.cs
@@ -0,0 +1,17 @@
+﻿using DigitalImageProcessingLib.ImageType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.Interface
+{
+    public enum UnifyingFeature { StrokeWidth }
+    interface IConnectedComponent
+    {
+        UnifyingFeature Feature { get; set; }
+        void FindComponents(GreyImage image);
+        void FindComponents(RGBImage image);
+    }
+}
