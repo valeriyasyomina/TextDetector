ce2e036bf76ff3f514f57fef325b8b1e522285ff
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-19 16:41:19 +0300

    RegionNumber data added

--- src/DigitalImageProcessingLib/PixelData/PixelData.cs
+++ src/DigitalImageProcessingLib/PixelData/PixelData.cs
@@ -12,16 +12,19 @@ namespace DigitalImageProcessingLib.PixelData
 {    
     public class PixelData<ColorType> where ColorType: ColorBase, new()
     {
+        public static int UNDEFINED_REGION = -1;
         public PixelData()
         {
             this.Gradient = new Gradient() { Strength = 0, Angle = 0, RoundGradientDirection = RoundGradientDirection.UNDEFINED };
             this.BorderType = Border.UNDEFINED;         
             this.Color = new ColorType();
             this.StrokeWidth = new StrokeWidthData();
+            this.RegionNumber = UNDEFINED_REGION;
         }
         public Gradient Gradient { get; set; }
         public Border BorderType { get; set; }
         public ColorType Color { get; set; }
         public StrokeWidthData StrokeWidth { get; set; }
+        public int RegionNumber { get; set; }
     }
 }
