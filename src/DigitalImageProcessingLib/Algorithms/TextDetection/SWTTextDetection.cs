7308e07d3bc39ce117213fd6c8bb0b9550e4a82e
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-22 13:40:30 +0300

    TextDetection class added

---
+++ src/DigitalImageProcessingLib/Algorithms/TextDetection/SWTTextDetection.cs
@@ -0,0 +1,23 @@
+﻿using DigitalImageProcessingLib.ImageType;
+using DigitalImageProcessingLib.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.Algorithms.TextDetection
+{
+    public class SWTTextDetection: ITextDetection
+    {
+        public void DetectText(GreyImage image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DetectText(RGBImage image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/DigitalImageProcessingLib/Interface/ITextDetection.cs
+++ src/DigitalImageProcessingLib/Interface/ITextDetection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DigitalImageProcessingLib.ImageType;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace DigitalImageProcessingLib.Interface
 {
     interface ITextDetection
     {
+        void DetectText(GreyImage image);
+        void DetectText(RGBImage image);
     }
 }
