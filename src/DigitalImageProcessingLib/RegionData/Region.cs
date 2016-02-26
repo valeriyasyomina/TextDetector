8d2cd63426ee1f9a97550348e867872dd9086d56
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-26 13:50:02 +0300

    Region class added

---
+++ src/DigitalImageProcessingLib/RegionData/Region.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.RegionData
+{
+    public class Region
+    {
+        public int PixelsNumber { get; set; }
+        public int AverageStrokeWidth { get; set; }
+        public int MaxBorderIndexI { get; set; }
+        public int MaxBorderIndexJ { get; set; }        
+        public int MinBorderIndexI { get; set; }
+        public int MinBorderIndexJ { get; set; }
+    }
+}
