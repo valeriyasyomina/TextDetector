ef8e4b786ed6773fa9adb0034267ed53f10394a5
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-02-28 20:46:59 +0300

    Region data added

--- src/DigitalImageProcessingLib/RegionData/Region.cs
+++ src/DigitalImageProcessingLib/RegionData/Region.cs
@@ -10,9 +10,14 @@ namespace DigitalImageProcessingLib.RegionData
     {
         public int PixelsNumber { get; set; }
         public int AverageStrokeWidth { get; set; }
+        public int SummaryStrokeWidth { get; set; }
         public int MaxBorderIndexI { get; set; }
         public int MaxBorderIndexJ { get; set; }        
         public int MinBorderIndexI { get; set; }
         public int MinBorderIndexJ { get; set; }
+        public int TruePixelsNumber { get; set; }
+        public int Square { get; set; }
+        public int MaxStrokeWidth { get; set; }
+        public int MinStrokeWidth { get; set; }
     }
 }
