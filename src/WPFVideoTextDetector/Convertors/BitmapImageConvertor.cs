5fec8204034a807e1f7db5b938aad353f8148437
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-03-27 20:54:54 +0300

    Bitmap to BitmapImage convertor added

---
+++ src/WPFVideoTextDetector/Convertors/BitmapImageConvertor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WPFVideoTextDetector.Convertors
+{
+    public class BitmapImageConvertor: IImageConvertor
+    {
+        /// <summary>
+        /// Конвертация bitmap в bitmapImage
+        /// </summary>
+        /// <param name="bitmap">Bitmap</param>
+        /// <returns>bitmapImage</returns>
+        public BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {           
+            MemoryStream memoryStream = new MemoryStream();
+            ((System.Drawing.Bitmap)bitmap).Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = memoryStream;
+            image.EndInit();
+            return image; 
+        }
+    }
+}
---
+++ src/WPFVideoTextDetector/Convertors/IImageConvertor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WPFVideoTextDetector.Convertors
+{
+    public interface IImageConvertor
+    {
+        BitmapImage BitmapToBitmapImage(Bitmap bitmap);
+    }
+}
