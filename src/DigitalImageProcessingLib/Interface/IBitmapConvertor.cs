24286df5f61e190bed3f34f0039801faa99146c9
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-03-29 18:34:50 +0300

    Ibit,apConvertor added

---
+++ src/DigitalImageProcessingLib/Interface/IBitmapConvertor.cs
@@ -0,0 +1,18 @@
+﻿using DigitalImageProcessingLib.ImageType;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.Interface
+{
+    interface IBitmapConvertor
+    {
+        Bitmap ToBitmap(GreyImage image);
+        Bitmap ToBitmap(RGBImage image);
+        GreyImage ToGreyImage(Bitmap bitmap);
+        RGBImage ToRGBImage(Bitmap bitmap);
+    }
+}
