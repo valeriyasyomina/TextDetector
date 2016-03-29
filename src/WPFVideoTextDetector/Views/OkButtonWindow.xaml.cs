54ed41d765f59c1af6956a2c33bd92615afa164e
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-03-29 16:31:25 +0300

    Init functiona added

--- src/WPFVideoTextDetector/Views/OkButtonWindow.xaml.cs
+++ src/WPFVideoTextDetector/Views/OkButtonWindow.xaml.cs
@@ -19,6 +19,10 @@ namespace WPFVideoTextDetector.Views
     /// </summary>
     public partial class OkButtonWindow : Window
     {
+        private static string INFO_TEXT_DEFAULT = "";
+        private static int OK_BUTTON_WINDOW_HEADER_TEXT_PADDING_DEFAULT = 10;
+        private static int OK_BUTTON_WINDOW_TEXT_PADDING_DEFAULT = 5;       
+
         public OkButtonWindow()
         {
             InitializeComponent();
@@ -27,6 +31,26 @@ namespace WPFVideoTextDetector.Views
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        /// <summary>
+        /// Инициализация экземпляра окна
+        /// </summary>
+        /// <returns></returns>
+        public static OkButtonWindow InitializeOkButtonWindow()
+        {
+            OkButtonWindow okButtonWindow = new OkButtonWindow();
+            okButtonWindow.capitalText.Background = (System.Windows.Media.Brush)Application.Current.MainWindow.FindResource("Brush8B4789");
+            okButtonWindow.capitalText.Padding = new Thickness(OK_BUTTON_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_HEADER_TEXT_PADDING_DEFAULT);
+            okButtonWindow.textInformation.Padding = new Thickness(OK_BUTTON_WINDOW_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_TEXT_PADDING_DEFAULT,
+                                                                OK_BUTTON_WINDOW_TEXT_PADDING_DEFAULT);
+            okButtonWindow.textInformation.Text = INFO_TEXT_DEFAULT;
+            return okButtonWindow;
         }
     }
 }
--- src/WPFVideoTextDetector/Views/ProgressWindow.xaml.cs
+++ src/WPFVideoTextDetector/Views/ProgressWindow.xaml.cs
@@ -19,9 +19,33 @@ namespace WPFVideoTextDetector.Views
     /// </summary>
     public partial class ProgressWindow : Window
     {
+        private static string INFO_TEXT_DEFAULT = "";
+        private static int PROGRESS_WINDOW_HEADER_TEXT_PADDING_DEFAULT = 10;
+        private static int PROGRESS_WINDOW_TEXT_PADDING_DEFAULT = 5;
+        private static double PROGRESS_BAR_STEP_DEFAULT = 0.5;
+
         public ProgressWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Инициализация экземпляра окна с pgorgess bar
+        /// </summary>
+        /// <returns></returns>
+        public static ProgressWindow InitializeProgressWindow()
+        {
+            ProgressWindow progressWindow = new ProgressWindow();
+            progressWindow.capitalText.Background = (System.Windows.Media.Brush)Application.Current.MainWindow.FindResource("Brush8B4789");
+            progressWindow.capitalText.Padding = new Thickness(PROGRESS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                PROGRESS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                PROGRESS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                PROGRESS_WINDOW_HEADER_TEXT_PADDING_DEFAULT);
+            progressWindow.pbStatus.Value = 0;
+            progressWindow.pbStatus.SmallChange = PROGRESS_BAR_STEP_DEFAULT;
+            progressWindow.textInformation.Text = INFO_TEXT_DEFAULT;
+
+            return progressWindow;
         }
     }
 }
