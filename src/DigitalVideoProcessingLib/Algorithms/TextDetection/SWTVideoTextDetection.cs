219bd56b15ca626c1eab8cf4d875693e679434af
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-03-29 18:31:05 +0300

    Video text detection func added

--- src/DigitalVideoProcessingLib/Algorithms/TextDetection/SWTVideoTextDetection.cs
+++ src/DigitalVideoProcessingLib/Algorithms/TextDetection/SWTVideoTextDetection.cs
@@ -1,4 +1,7 @@
-﻿using DigitalVideoProcessingLib.Interface;
+﻿using DigitalImageProcessingLib.Algorithms.TextDetection;
+using DigitalImageProcessingLib.Interface;
+using DigitalVideoProcessingLib.Interface;
+using DigitalVideoProcessingLib.VideoFrameType;
 using DigitalVideoProcessingLib.VideoType;
 using System;
 using System.Collections.Generic;
@@ -8,11 +11,77 @@ using System.Threading.Tasks;
 
 namespace DigitalVideoProcessingLib.Algorithms.TextDetection
 {
-    public class SWTVideoTextDetection: ITextDetection
+    public class SWTVideoTextDetection : DigitalVideoProcessingLib.Interface.ITextDetection
     {
-        public void DetectText(GreyVideo video)
+        public IEdgeDetection EdgeDetector { get; set; }
+        public int SWTPixelsDelta { get; set; }
+        public double PixelsStrokeWidthTreshold { get; set; }
+        public int MinimumPixelsNumberInRegion { get; set; }
+        public SWTVideoTextDetection(IEdgeDetection edgeDetector, int SWTPixelsDelta, double pixelsStrokeWidthTreshold,
+            int minimumPixelsNumberInRegion)
         {
-            throw new NotImplementedException();
+            if (edgeDetector == null)
+                throw new ArgumentNullException("Null edgeDetector in SWTVideoTextDetection");
+            this.EdgeDetector = edgeDetector;
+            this.SWTPixelsDelta = SWTPixelsDelta;
+            this.PixelsStrokeWidthTreshold = pixelsStrokeWidthTreshold;
+            this.MinimumPixelsNumberInRegion = minimumPixelsNumberInRegion;
+        }
+
+        /// <summary>
+        /// Выделение текста на кд\лючевых кадрах видеоролика
+        /// </summary>
+        /// <param name="video">Видеоролик</param>
+        /// <returns>true</returns>
+        public Task<bool> DetectText(GreyVideo video)
+        {
+            try
+            {
+                if (video == null)
+                    throw new ArgumentNullException("Null video in DetectText");
+                if (video.Frames == null)
+                    throw new ArgumentNullException("Null video frames in DetectText");
+
+                SWTTextDetection SWTTextDetection = new SWTTextDetection(this.EdgeDetector, this.SWTPixelsDelta, this.PixelsStrokeWidthTreshold,
+                                                                    this.MinimumPixelsNumberInRegion);
+
+                return Task.Run(() =>
+                {
+                    for (int i = 0; i < video.Frames.Count; i++)
+                        SWTTextDetection.DetectText(video.Frames[i].Frame);
+                   return true;
+                });
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// Выделение текста на единичном кадре видеопотока
+        /// </summary>
+        /// <param name="videoFrame">Кадр видео</param>
+        /// <returns>true</returns>
+        public Task<bool> DetectText(GreyVideoFrame videoFrame)
+        {
+            try
+            {
+                if (videoFrame == null || videoFrame.Frame == null)
+                    throw new ArgumentNullException("Null frame in DetectText");
+
+                SWTTextDetection SWTTextDetection = new SWTTextDetection(this.EdgeDetector, this.SWTPixelsDelta, this.PixelsStrokeWidthTreshold,
+                                                                    this.MinimumPixelsNumberInRegion);
+                return Task.Run(() =>
+                {
+                        SWTTextDetection.DetectText(videoFrame.Frame);
+                        return true;
+                });
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
         }
     }
 }
