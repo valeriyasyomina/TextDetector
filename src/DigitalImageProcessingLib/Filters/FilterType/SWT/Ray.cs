9ab929711e9b90f5a1101769a4886e987a55cb67
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-03-30 20:26:43 +0300

    Ray class added for swt filter

---
+++ src/DigitalImageProcessingLib/Filters/FilterType/SWT/Ray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.Filters.FilterType.SWT
+{
+    public enum RayDirection { RIGHT, LEFT, DOWN, UP, RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN, UNDEFINED }
+    public class Ray
+    {
+        public Ray()
+        {
+            this.Direction = RayDirection.UNDEFINED;
+        }
+        public int RowBeginIndex { get; set; }
+        public int RowEndIndex { get; set; }
+        public int ColumnBeginIndex { get; set; }
+        public int ColumnEndIndex { get; set; }
+        public int ColumnStep { get; set; }
+        public RayDirection Direction { get; set; }
+
+    }
+}
