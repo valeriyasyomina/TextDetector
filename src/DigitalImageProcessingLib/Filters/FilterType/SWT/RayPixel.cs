441de3f58d8804e42ffb0af68761243666e6c81b
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-01 11:04:36 +0300

    RayPixel class added

---
+++ src/DigitalImageProcessingLib/Filters/FilterType/SWT/RayPixel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalImageProcessingLib.Filters.FilterType.SWT
+{
+    public class RayPixel
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public double StrokeWidth { get; set; }
+    }
+}
