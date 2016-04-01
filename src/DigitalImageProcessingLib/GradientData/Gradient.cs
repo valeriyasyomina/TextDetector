9da851917d838219012644f344aa186eaa7c9131
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-01 11:04:14 +0300

    fields for swt added

--- src/DigitalImageProcessingLib/GradientData/Gradient.cs
+++ src/DigitalImageProcessingLib/GradientData/Gradient.cs
@@ -9,8 +9,14 @@ namespace DigitalImageProcessingLib.GradientData
     public enum RoundGradientDirection {DEGREE_0 = 0, DEGREE__45 = 45, DEGREE_90 = 90, DEGREE_135 = 135, UNDEFINED }
     public class Gradient
     {
+        public static double UNDEFINED_VALUE = -1.0;
         public int Strength { get; set; }
         public int Angle { get; set; }
         public RoundGradientDirection RoundGradientDirection { get; set; }
+        public double GradientX { get; set; }
+        public double GradientY { get; set; }
+        public double StepX { get; set; }
+        public double StepY { get; set; }
+        public double Magnitude { get; set; }
     }
 }
