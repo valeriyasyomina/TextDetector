7b1f9003cd12b7cca3e07a7ac7497873c39477a3
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-15 08:05:22 +0300

    key frames info list added

--- src/DigitalVideoProcessingLib/IO/IOData.cs
+++ src/DigitalVideoProcessingLib/IO/IOData.cs
@@ -8,8 +8,13 @@ namespace DigitalVideoProcessingLib.IO
 {
     public class IOData
     {
+        public IOData()
+        {
+            this.KeyFrameIOInformation = new List<KeyFrameIOInformation>();
+        }
         public string FileName { get; set; }
         public int FrameWidth { get; set; }
         public int FrameHeight { get; set; }
+        public List<KeyFrameIOInformation> KeyFrameIOInformation { get; set; }
     }
 }
