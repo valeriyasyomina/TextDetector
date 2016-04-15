64838ae74cd6e7b1b9cfb3c80dfec06e68ded44d
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-15 08:03:50 +0300

    keyframeinformation class added

---
+++ src/DigitalVideoProcessingLib/IO/KeyFrameIOInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalVideoProcessingLib.IO
+{
+    public class KeyFrameIOInformation
+    {
+        public int Number { get; set; }
+        public bool NeedProcess { get; set; }
+    }
+}
