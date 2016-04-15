b87dd28db3c4b3e2e32960a723ed27a4ecab7468
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-15 08:19:03 +0300

    Draw class added

---
+++ src/DigitalVideoProcessingLib/Graphics/Draw.cs
@@ -0,0 +1,39 @@
+﻿using DigitalImageProcessingLib.RegionData;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalVideoProcessingLib.Graphics
+{
+    public class Draw
+    {
+        public static void DrawTextBoundingBoxes(Bitmap bitmapFrame, List<TextRegion> textRegions, System.Drawing.Pen pen)
+        {
+            try
+            {
+                if (bitmapFrame == null)
+                    throw new ArgumentNullException("Null bitmap frame in DrawTextBoundingBoxes");
+                if (textRegions == null)
+                    throw new ArgumentNullException("Null textRegions in DrawTextBoundingBoxes");
+                if (pen == null)
+                    throw new ArgumentNullException("Null pen in DrawTextBoundingBoxes");
+
+                System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmapFrame);
+
+                if (textRegions != null)
+                {
+                    for (int i = 0; i < textRegions.Count; i++)
+                        graphics.DrawRectangle(pen, textRegions[i].MinBorderIndexJ, textRegions[i].MinBorderIndexI,
+                            textRegions[i].MaxBorderIndexJ - textRegions[i].MinBorderIndexJ, textRegions[i].MaxBorderIndexI - textRegions[i].MinBorderIndexI);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
