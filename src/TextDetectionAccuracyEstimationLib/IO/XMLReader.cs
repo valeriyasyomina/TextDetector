8d64d69dad196883f6a256d4715378eaca58fd2a
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-19 22:14:19 +0300

    xml reader writer added

---
+++ src/TextDetectionAccuracyEstimationLib/IO/XMLReader.cs
@@ -0,0 +1,104 @@
+﻿using DigitalImageProcessingLib.RegionData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TextDetectionAccuracyEstimationLib.IO
+{
+    public class XMLReader
+    {
+        /// <summary>
+        ///  Считывание информации о тексчтовых блоках из xml - файла
+        /// </summary>
+        /// <param name="XMLFileName">Имя Xml - файла</param>
+        /// <returns></returns>
+        public static Task<Dictionary<int, List<TextRegion>>> ReadTextBlocksInformation(string XMLFileName)
+        {
+            try
+            {
+                if (XMLFileName == null || XMLFileName.Length == 0)
+                    throw new ArgumentNullException("Null XMLFileName in ReadTextBlocksInformation");
+
+                return Task.Run(() =>
+                {
+                    Dictionary<int, List<TextRegion>> framesTextBlocksInformation = new Dictionary<int, List<TextRegion>>();
+                    System.Xml.XmlReader xmlReader = System.Xml.XmlReader.Create(XMLFileName);                   
+                    while (xmlReader.Read())
+                    {
+                         if (xmlReader.Name == "Frame")
+                         {
+                             List<TextRegion> textRegions = null;
+                             int frameNumber = 0;
+                             ReadFrameTextBlocksInformation(xmlReader, out textRegions, out frameNumber);
+                             framesTextBlocksInformation.Add(frameNumber, textRegions);
+                         }
+                    }
+                    return framesTextBlocksInformation;
+                });
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+        /// <summary>
+        /// Считывание текстовых областей для одного кадра
+        /// </summary>
+        /// <param name="xmlReader">xml - reader</param>
+        /// <param name="textRegions">Текстовые области</param>
+        /// <param name="frameNumber">Номер кадра</param>
+        private static void ReadFrameTextBlocksInformation(System.Xml.XmlReader xmlReader, out List<TextRegion> textRegions,
+            out int frameNumber)
+        {
+            try
+            {
+                textRegions = new List<TextRegion>();          
+                xmlReader.MoveToFirstAttribute();
+                frameNumber = Convert.ToInt32(xmlReader.Value);
+                xmlReader.MoveToNextAttribute();
+                int textRegionsNumber = Convert.ToInt32(xmlReader.Value);
+                if (textRegionsNumber != 0)
+                {
+                    bool readingFrame = true;
+                    while (xmlReader.Read() && readingFrame)
+                    {
+                        if (xmlReader.Name == "Frame")
+                            readingFrame = false;
+                        else if (xmlReader.Name == "TextRegion")
+                        {
+                            TextRegion textRegion = new TextRegion();
+                            while (xmlReader.MoveToNextAttribute())
+                            {
+                                switch (xmlReader.Name)
+                                {
+                                    case "LeftUpPointIndexI":
+                                        textRegion.MinBorderIndexI = Convert.ToInt32(xmlReader.Value);
+                                        break;
+                                    case "LeftUpPointIndexJ":
+                                        textRegion.MinBorderIndexJ = Convert.ToInt32(xmlReader.Value);
+                                        break;
+                                    case "RightDownPointIndexI":
+                                        textRegion.MaxBorderIndexI = Convert.ToInt32(xmlReader.Value);
+                                        break;
+                                    case "RightDownPointIndexJ":
+                                        textRegion.MaxBorderIndexJ = Convert.ToInt32(xmlReader.Value);
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            textRegions.Add(textRegion);
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
---
+++ src/TextDetectionAccuracyEstimationLib/IO/XMLWriter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using TextDetectionAccuracyEstimationLib.AccuracyEstimation;
+using TextDetectionAccuracyEstimationLib.Metrics;
+
+namespace TextDetectionAccuracyEstimationLib.IO
+{
+    public class XMLWriter
+    {
+        /// <summary>
+        /// Запись вычисленных метрик в xml - файл
+        /// </summary>
+        /// <param name="XMLFileName">Имя файла</param>
+        /// <param name="metrics">Метрики</param>
+        /// <returns></returns>
+        public static Task WriteMetricsXML(string XMLFileName, Dictionary<int, List<Metric>> metrics)
+        {
+            try
+            {
+                if (XMLFileName == null || XMLFileName.Length == 0)
+                    throw new ArgumentNullException("Null XMLFileName in WriteMetricsXML");
+                return Task.Run(() =>
+                {
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Indent = true;
+                    settings.IndentChars = "\t";
+                    System.Xml.XmlWriter xmlWriter = System.Xml.XmlWriter.Create(XMLFileName, settings);
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("Metrics");
+
+                    foreach (var pair in metrics)
+                    {
+                        if (pair.Key != AccuracyEstimator.VIDEO_METRICS)
+                            WriteOneFrameMetrics(xmlWriter, pair.Key, pair.Value);
+                        else
+                            FriteVideoMetrics(xmlWriter, pair.Value);
+                    }
+                    xmlWriter.WriteEndDocument();
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                });
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+        /// <summary>
+        /// Запись метрик для видео в файл
+        /// </summary>
+        /// <param name="xmlWriter">Xml - writer</param>
+        /// <param name="metricsForFrame">Метрики</param>
+        private static void FriteVideoMetrics(System.Xml.XmlWriter xmlWriter, List<Metric> metricsForFrame)
+        {
+            try
+            {
+                xmlWriter.WriteStartElement("Video");            
+
+                WriteMetricsArray(xmlWriter, metricsForFrame);
+
+                xmlWriter.WriteEndElement();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+        /// <summary>
+        /// Запись метрик в файл для одного кадра видео
+        /// </summary>
+        /// <param name="xmlWriter">xml - writer</param>
+        /// <param name="frameNumber">Номер кадра</param>
+        /// <param name="metricsForFrame">Метрики</param>
+        private static void WriteOneFrameMetrics(System.Xml.XmlWriter xmlWriter, int frameNumber, List<Metric> metricsForFrame)
+        {
+            try
+            {
+                xmlWriter.WriteStartElement("Frame");
+                xmlWriter.WriteAttributeString("ID", frameNumber.ToString());
+
+                WriteMetricsArray(xmlWriter, metricsForFrame);
+                
+                xmlWriter.WriteEndElement();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+        /// <summary>
+        /// Запись массива метрик в файл
+        /// </summary>
+        /// <param name="xmlWriter">xml - writer</param>
+        /// <param name="metricsForFrame">Метрики</param>
+        private static void WriteMetricsArray(System.Xml.XmlWriter xmlWriter, List<Metric> metricsForFrame)
+        {
+            try
+            {
+                for (int i = 0; i < metricsForFrame.Count; i++)
+                {
+                    if (metricsForFrame[i].GetType() == typeof(SecondTypeErrorProbability))
+                    {
+                        xmlWriter.WriteStartElement("SecondTypeErrorProbability");
+                        if (metricsForFrame[i].Value == Metric.UNDEFINED_METRIC)
+                            xmlWriter.WriteString("UNDEFINED_METRIC");
+                        else
+                            xmlWriter.WriteString(metricsForFrame[i].Value.ToString());
+                        xmlWriter.WriteEndElement();
+                    }
+                    else if (metricsForFrame[i].GetType() == typeof(FirstTypeErrorProbability))
+                    {
+                        xmlWriter.WriteStartElement("FirstTypeErrorProbability");
+                        if (metricsForFrame[i].Value == Metric.UNDEFINED_METRIC)
+                            xmlWriter.WriteString("UNDEFINED_METRIC");
+                        else
+                            xmlWriter.WriteString(metricsForFrame[i].Value.ToString());
+                        xmlWriter.WriteEndElement();
+                    }
+                    else if (metricsForFrame[i].GetType() == typeof(Precision))
+                    {
+                        xmlWriter.WriteStartElement("Precision");
+                        if (metricsForFrame[i].Value == Metric.UNDEFINED_METRIC)
+                            xmlWriter.WriteString("UNDEFINED_METRIC");
+                        else
+                            xmlWriter.WriteString(metricsForFrame[i].Value.ToString());
+                        xmlWriter.WriteEndElement();
+                    }
+                    else if (metricsForFrame[i].GetType() == typeof(Recall))
+                    {
+                        xmlWriter.WriteStartElement("Recall");
+                        if (metricsForFrame[i].Value == Metric.UNDEFINED_METRIC)
+                            xmlWriter.WriteString("UNDEFINED_METRIC");
+                        else
+                            xmlWriter.WriteString(metricsForFrame[i].Value.ToString());
+                        xmlWriter.WriteEndElement();
+                    }
+                    else if (metricsForFrame[i].GetType() == typeof(F1Measure))
+                    {
+                        xmlWriter.WriteStartElement("F1Measure");
+                        if (metricsForFrame[i].Value == Metric.UNDEFINED_METRIC)
+                            xmlWriter.WriteString("UNDEFINED_METRIC");
+                        else
+                            xmlWriter.WriteString(metricsForFrame[i].Value.ToString());
+                        xmlWriter.WriteEndElement();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
