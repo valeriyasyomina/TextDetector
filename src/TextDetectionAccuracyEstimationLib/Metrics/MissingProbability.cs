f2225167a7eb4166e2fbb1616a088f3e5d22dbab
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-20 09:44:25 +0300

    missimg metric added

---
+++ src/TextDetectionAccuracyEstimationLib/Metrics/MissingProbability.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDetectionAccuracyEstimationLib.Metrics
+{
+    public class MissingProbability: Metric
+    {
+        public MissingProbability() { }
+        public MissingProbability(double value)
+        {
+            this.Value = value;
+        }
+        public override double Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
