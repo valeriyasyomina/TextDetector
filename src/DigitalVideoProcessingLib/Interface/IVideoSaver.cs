3f8083e5937c19b52c0b13794ab724dc120fb81a
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-24 21:22:53 +0300

    save method for bitmap array array

--- src/DigitalVideoProcessingLib/Interface/IVideoSaver.cs
+++ src/DigitalVideoProcessingLib/Interface/IVideoSaver.cs
@@ -12,6 +12,7 @@ namespace DigitalVideoProcessingLib.Interface
     public interface IVideoSaver
     {
         Task<bool> SaveVideoAsync(GreyVideo video, System.Drawing.Pen pen, string pathToSave, string framesSubDir, string framesExpansion);
+        Task<bool> SaveVideoAsync(List<Bitmap> bitmapFrames, string pathToSave, string framesSubDir, string framesExpansion);
         Task<bool> SaveVideoFrameAsync(GreyVideoFrame videoFrame, System.Drawing.Pen pen, string saveFileName);
     }
 }
