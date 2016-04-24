a292c8a77e5ea49677b5b0846c1b8f4f642d6f4c
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-24 19:05:54 +0300

    mediator for video loading and processing simulteniously added

---
+++ src/DigitalVideoProcessingLib/Mediators/LoadDetectTextVideoMediator.cs
@@ -0,0 +1,43 @@
+﻿using DigitalVideoProcessingLib.Algorithms.TextDetection;
+using DigitalVideoProcessingLib.IO;
+using DigitalVideoProcessingLib.VideoFrameType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalVideoProcessingLib.Mediators
+{
+    public delegate void FrameWasProcessed(GreyVideoFrame videoFrame);
+    public class LoadDetectTextVideoMediator
+    {
+        public static event FrameWasProcessed FrameWasProcessedEvent;
+        public async static Task LoadDetectTextVideo(SWTVideoTextDetection sWTVideoTextDetection, FrameLoader frameLoader,
+            List<KeyFrameIOInformation> keyFrameIOInformation, string videoFileName, int threadsNumber)
+        {
+            try
+            {
+                if (sWTVideoTextDetection == null)
+                    throw new ArgumentNullException("Null sWTVideoTextDetection in LoadDetectTextVideo");
+                if (frameLoader == null)
+                    throw new ArgumentNullException("Null frameLoader in LoadDetectTextVideo");
+                if (keyFrameIOInformation == null)
+                    throw new ArgumentNullException("Null keyFrameIOInformation in LoadDetectTextVideo");
+                if (videoFileName == null)
+                    throw new ArgumentNullException("Null videoFileName in LoadDetectTextVideo");              
+
+                for (int i = 0; i < keyFrameIOInformation.Count; i++)
+                {
+                    GreyVideoFrame frame = await frameLoader.LoadFrameAsync(videoFileName, keyFrameIOInformation[i]);
+                    await sWTVideoTextDetection.DetectText(frame, threadsNumber);
+                    FrameWasProcessedEvent(frame);
+                }               
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
