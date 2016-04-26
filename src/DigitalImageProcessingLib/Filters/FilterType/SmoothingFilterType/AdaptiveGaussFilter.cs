f78bd1963bae491d70574f625514c1ced370dd51
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-26 18:55:34 +0300

    parallel realization added

--- src/DigitalImageProcessingLib/Filters/FilterType/SmoothingFilterType/AdaptiveGaussFilter.cs
+++ src/DigitalImageProcessingLib/Filters/FilterType/SmoothingFilterType/AdaptiveGaussFilter.cs
@@ -1,9 +1,11 @@
 ﻿using DigitalImageProcessingLib.ColorType;
 using DigitalImageProcessingLib.ImageType;
+using DigitalImageProcessingLib.ThreadData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DigitalImageProcessingLib.Filters.FilterType.SmoothingFilterType
@@ -11,6 +13,7 @@ namespace DigitalImageProcessingLib.Filters.FilterType.SmoothingFilterType
     public class AdaptiveGaussFilter: SmoothingFilter
     {
         private static int DEFAULT_ITERATIONS_NUMBER = 1;
+        private GreyImage _copyImage = null;
         public double Sigma { get; set; }
         public int IterationsNumber { get; set; }
         public AdaptiveGaussFilter(double sigma, int iterationNumber = 1) 
@@ -132,12 +135,119 @@ namespace DigitalImageProcessingLib.Filters.FilterType.SmoothingFilterType
 
         public override GreyImage Apply(GreyImage image, int threadsNumber)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (image == null)
+                    throw new ArgumentNullException("Null image in Apply");
+                if (image.Height < this.Size)
+                    throw new ArgumentException("Image height must be >= filter size");
+                if (image.Width < this.Size)
+                    throw new ArgumentException("Image width must be >= filter size");
+                if (threadsNumber <= 0)
+                    throw new ArgumentException("Error threadsNumber in Apply");
+
+                this._copyImage = (GreyImage)image.Copy();
+                if (this._copyImage == null)
+                    throw new NullReferenceException("Null copy image in Apply");
+
+                this.Threads = new List<Thread>();
+
+                int deltaI = image.Height / threadsNumber;
+                int lowIndex = 1;
+                int lowIndexI = 1;           
+                int highIndexI = lowIndexI + deltaI;       
+                int highIndexJ = image.Width - 2;
+                
+                for (int i = 0; i < threadsNumber; i++)
+                {
+                    if (i == threadsNumber - 1)
+                        highIndexI = image.Height - 2;                  
+
+                    MatrixFilterData matrixFilterData = new ThreadData.MatrixFilterData(image, lowIndexI, highIndexI, lowIndex, highIndexJ);
+                    Thread thread = new Thread(new ParameterizedThreadStart(this.ApplyThread));
+                    this.Threads.Add(thread);
+                    this.Threads[i].Start(matrixFilterData);                     
+                  
+                    lowIndexI = highIndexI;
+                    highIndexI += deltaI;
+                }
+                WaitForThreads();
+
+                return this._copyImage;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// Ожидание завершения потоков
+        /// </summary>
+        private void WaitForThreads()
+        {
+            try
+            {
+                int hreadsNumber = this.Threads.Count;
+                for (int i = 0; i < hreadsNumber; i++)
+                    this.Threads[i].Join();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
         }
 
         protected override void ApplyThread(object data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                MatrixFilterData matrixFilterData = (MatrixFilterData)data;
+
+                GreyImage image = matrixFilterData.GreyImage;
+                int startI = matrixFilterData.StartIndexI;
+                int endI = matrixFilterData.EndIndexI;
+                int startJ = matrixFilterData.StartIndexJ;
+                int endJ = matrixFilterData.EndIndexJ;
+                int filterSize = this.Size;
+                int lowIndex = filterSize / 2;
+
+                for (int i = startI; i < endI; i++)
+                    for (int j = startJ; j < endJ; j++)
+                    {
+                        double w1 = CalculateWeight(image, i - 1, j - 1);
+                        double w2 = CalculateWeight(image, i - 1, j);
+                        double w3 = CalculateWeight(image, i - 1, j + 1);
+                        double w4 = CalculateWeight(image, i, j - 1);
+                        double w5 = CalculateWeight(image, i, j + 1);
+                        double w6 = CalculateWeight(image, i + 1, j - 1);
+                        double w7 = CalculateWeight(image, i + 1, j);
+                        double w8 = CalculateWeight(image, i + 1, j + 1);
+                        double wCurrentPixel = CalculateWeight(image, i, j);
+
+                        double N = w1 + w2 + w3 + w4 + w5 + w6 + w7 + w8 + wCurrentPixel;
+                        double newPixelValue = w1 * image.Pixels[i - 1, j - 1].Color.Data + w2 * image.Pixels[i - 1, j].Color.Data +
+                            w3 * image.Pixels[i - 1, j + 1].Color.Data + w4 * image.Pixels[i, j - 1].Color.Data +
+                            w5 * image.Pixels[i, j + 1].Color.Data + w6 * image.Pixels[i + 1, j - 1].Color.Data +
+                            w7 * image.Pixels[i + 1, j].Color.Data + w8 * image.Pixels[i + 1, j + 1].Color.Data +
+                            wCurrentPixel * image.Pixels[i, j].Color.Data;
+                        if (N == 0.0)
+                            image.Pixels[i, j].Color.Data = (byte)ColorBase.MIN_COLOR_VALUE;
+                        else
+                        {
+                            int newColor = (int)(newPixelValue / N);
+                            if (newColor > ColorBase.MAX_COLOR_VALUE)
+                                newColor = ColorBase.MAX_COLOR_VALUE;
+                            if (newColor < ColorBase.MIN_COLOR_VALUE)
+                                newColor = ColorBase.MIN_COLOR_VALUE;
+                            this._copyImage.Pixels[i, j].Color.Data = (byte)newColor;
+                        }
+                    }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
         }
     }
 }
