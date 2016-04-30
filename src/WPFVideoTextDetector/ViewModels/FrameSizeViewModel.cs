b0ac54da299718b45a23469c9b1e5fe8e1252800
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-30 08:14:54 +0300

    frame size window added

---
+++ src/WPFVideoTextDetector/ViewModels/FrameSizeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFVideoTextDetector.Views;
+
+namespace WPFVideoTextDetector.ViewModels
+{
+    public class FrameSizeViewModel: ViewModelBase
+    {
+        private FrameSizeWindow window = null;
+        private int _frameWidth = 640;
+        private int _frameHeight = 480;
+
+        public int FrameWidth
+        {
+            get
+            {
+                return this._frameWidth;
+            }
+            set
+            {
+                this._frameWidth = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int FrameHeight
+        {
+            get
+            {
+                return this._frameHeight;
+            }
+            set
+            {
+                this._frameHeight = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public FrameSizeViewModel(FrameSizeWindow window)
+        {
+            if (window == null)
+                throw new ArgumentNullException("Null window in ctor FrameSizeViewModel");
+            this.window = window;
+        }
+    }
+}
---
+++ src/WPFVideoTextDetector/Views/FrameSizeWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFVideoTextDetector.ViewModels;
+
+namespace WPFVideoTextDetector.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для FrameSizeWidth.xaml
+    /// </summary>
+    public partial class FrameSizeWindow : Window
+    {
+        private static int FRAME_SIZE_WINDOW_HEADER_TEXT_PADDING_DEFAULT = 10;    
+        public FrameSizeWindow()
+        {
+            FrameSizeViewModel viewModel = new FrameSizeViewModel(this);
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Инициализация экземпляра окна
+        /// </summary>
+        /// <returns></returns>
+        public static FrameSizeWindow InitializeFrameSizeWindow()
+        {
+            FrameSizeWindow frameSizeWindow = new FrameSizeWindow();
+            frameSizeWindow.capitalText.Background = (System.Windows.Media.Brush)Application.Current.MainWindow.FindResource("Brush8B4789");
+            frameSizeWindow.capitalText.Padding = new Thickness(FRAME_SIZE_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                FRAME_SIZE_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                FRAME_SIZE_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                FRAME_SIZE_WINDOW_HEADER_TEXT_PADDING_DEFAULT);
+            return frameSizeWindow;
+        }
+    }
+}
