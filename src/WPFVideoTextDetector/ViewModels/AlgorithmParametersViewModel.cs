de77ac1885df3cbc15cf3f18c62f06e34d240dd1
valeriya.syomina@yandex.ru <valeriya.syomina@yandex.ru>
2016-04-30 08:16:23 +0300

    algorithm params window added

---
+++ src/WPFVideoTextDetector/ViewModels/AlgorithmParametersViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFVideoTextDetector.Views;
+
+namespace WPFVideoTextDetector.ViewModels
+{
+    public class AlgorithmParametersViewModel: ViewModelBase
+    {
+        private AlgorithmParametersWindow window = null;
+
+        private int _gaussFilterSize = 5;
+        private double _gaussSigma = 1.4;
+        private int _cannyLowTreshold = 20;
+        private int _cannyHighTreshold = 80;        
+
+        #region Properties
+        public int GaussFilterSize
+        {
+            get
+            {
+                return this._gaussFilterSize;
+            }
+            set
+            {
+                this._gaussFilterSize = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public double GaussSigma
+        {
+            get
+            {
+                return this._gaussSigma;
+            }
+            set
+            {
+                this._gaussSigma = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int CannyLowTreshold
+        {
+            get
+            {
+                return this._cannyLowTreshold;
+            }
+            set
+            {
+                this._cannyLowTreshold = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int CannyHighTreshold
+        {
+            get
+            {
+                return this._cannyHighTreshold;
+            }
+            set
+            {
+                this._cannyHighTreshold = value;
+                NotifyPropertyChanged();
+            }
+        }        
+        #endregion
+        public AlgorithmParametersViewModel(AlgorithmParametersWindow window)
+        {
+            if (window == null)
+                throw new ArgumentNullException("Null window in ctor AlgorithmParametersViewModel");
+            this.window = window;
+        }
+    }
+}
---
+++ src/WPFVideoTextDetector/Views/AlgorithmParametersWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFVideoTextDetector.ViewModels;
+
+namespace WPFVideoTextDetector.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AlgorithmParametersWindow.xaml
+    /// </summary>
+    public partial class AlgorithmParametersWindow : Window
+    {
+        private static int ALGORITM_PARAMS_WINDOW_HEADER_TEXT_PADDING_DEFAULT = 10;    
+        public AlgorithmParametersWindow()
+        {
+            AlgorithmParametersViewModel viewModel = new AlgorithmParametersViewModel(this);
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Инициализация экземпляра окна
+        /// </summary>
+        /// <returns></returns>
+        public static AlgorithmParametersWindow InitializeAlgorithmParametersWindow()
+        {
+            AlgorithmParametersWindow algorithmParametersWindow = new AlgorithmParametersWindow();
+            algorithmParametersWindow.capitalText.Background = (System.Windows.Media.Brush)Application.Current.MainWindow.FindResource("Brush8B4789");
+            algorithmParametersWindow.capitalText.Padding = new Thickness(ALGORITM_PARAMS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                ALGORITM_PARAMS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                ALGORITM_PARAMS_WINDOW_HEADER_TEXT_PADDING_DEFAULT,
+                                                                ALGORITM_PARAMS_WINDOW_HEADER_TEXT_PADDING_DEFAULT);
+            return algorithmParametersWindow;
+        }
+    }
+}
